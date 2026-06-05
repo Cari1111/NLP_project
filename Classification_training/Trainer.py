cf1f7cd3f8197f8112f48a25e58d9948fcfca1f8
Carina <dummy>
2026-06-05 15:51:41 +0200

    classification training

--- Classification_training/Trainer.py
+++ Classification_training/Trainer.py
@@ -2,45 +2,165 @@ import torch
 import numpy as np
 import pickle
 from dataclasses import dataclass
-from datasets import Dataset
-from transformers import BertTokenizer, BertForSequenceClassification
+from datasets import DatasetDict
+from datasets.arrow_dataset import Dataset
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification
+from sklearn.metrics import classification_report
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @dataclass
 class ClassificationTrainer:
     model: BertForSequenceClassification
-    ds: Dataset
+    ds: DatasetDict
     tokenizer: BertTokenizer
     identifier_str: str
     text_field: str = 'text'
     label_field: str = 'label'
+    train_split: str = 'train'
+    test_split: str = 'test'
+    evaluation_datasets: list[tuple[str, Dataset]] = None
+    model_lr: float = 1e-6
+    classifier_lr: float = 1e-4
 
     def __post_init__(self):
         self.model.to(device)
         self.loss_func = torch.nn.CrossEntropyLoss()
-        self.optimizer_network = torch.optim.Adam(self.model.bert.parameters(), lr=1e-6)
-        self.optimizer_head = torch.optim.Adam(self.model.classifier.parameters(), lr=1e-4)
+        self.optimizer_network = torch.optim.Adam(self.model.bert.parameters(), lr=self.model_lr)
+        self.optimizer_head = torch.optim.Adam(self.model.classifier.parameters(), lr=self.classifier_lr)
         self.losses = []
+        self.evaluation_datasets = self.evaluation_datasets if self.evaluation_datasets else []
+        self.evaluation_reports = {name: [] for name, _ in self.evaluation_datasets}
+    
+    def tokenize(self, batch):
+        return self.tokenizer(list(batch[self.text_field]), truncation=True, return_tensors='pt', padding=True).to(device)
+    
+    def evaluate_ds(self, dataset: Dataset):
+        self.model.eval()
+        predictions = []
+        for batch in dataset.batch(batch_size=16):
+            tokenized_text = self.tokenize(batch)
+            logits = self.model.forward(**tokenized_text).logits
+            print(logits.shape)
+            prediction = torch.argmax(logits, dim=1)
+            predictions.append(prediction)
+        all_predictions = torch.cat(predictions).cpu().numpy()
+        self.model.train()
 
+        print(classification_report(y_true=dataset[self.label_field], y_pred=all_predictions))
+        return classification_report(y_true=dataset[self.label_field], y_pred=all_predictions, output_dict=True)
+    
+    def evaluate_all(self):
+        for name, dataset in self.evaluation_datasets:
+            report_dict = self.evaluate_ds(dataset)
+            self.evaluation_reports[name].append(report_dict)
+        
     def save(self):
         with open(f'models/{self.identifier_str}-losses.dat', 'wb') as f:
             pickle.dump(self.losses, f)
+        with open(f'models/{self.identifier_str}-evaluations.dat', 'wb') as f:
+            pickle.dump(self.evaluation_reports, f)
         self.model.save_pretrained(f'models/{self.identifier_str}-model.dat')
 
     def train(self, episodes, batch_size=16):
+        self.evaluate_all()
         for episode in range(episodes):
-            print(f'--------------------- Episode {episode} ---------------------')
-            self.ds.shuffle()
-            for step, batch in enumerate(self.ds.batch(batch_size=batch_size)):
+            print(f'Episode {episode}')
+            episode_ds = self.ds[self.train_split].shuffle()
+            for step, batch in enumerate(   episode_ds.batch(batch_size=batch_size)):
                 tokenized_text = self.tokenizer(list(batch[self.text_field]), truncation=True, return_tensors='pt', padding=True).to(device)
                 labels = torch.tensor(batch[self.label_field]).to(device)
 
                 self.optimizer_network.zero_grad()
                 self.optimizer_head.zero_grad()
 
-                outputs = self.model(**tokenized_text, labels=labels)
+                outputs = self.model.forward(**tokenized_text, labels=labels)
+                print(outputs.logits.shape)
                 loss = outputs.loss
+
+                outputs.loss.backward()
+                self.optimizer_network.step()
+                self.optimizer_head.step()
+
+                print(step, loss.item(), end='\r')
+                self.losses.append(loss.item())
+            self.evaluate_all()
+            self.save()
+
+
+@dataclass
+class ClassificationTrainerMultInputs:
+    model: BertModel
+    classifier: torch.nn.Module
+    ds: DatasetDict
+    tokenizer: BertTokenizer
+    identifier_str: str
+    text_field: str|list[str] = 'text'
+    label_field: str = 'label'
+    train_split: str = 'train'
+    test_split: str = 'test'
+    model_lr: float = 1e-6
+    classifier_lr: float = 1e-4
+
+    def __post_init__(self):
+        self.model.to(device)
+        self.classifier.to(device)
+        self.loss_func = torch.nn.CrossEntropyLoss()
+        self.optimizer_network = torch.optim.Adam(self.model.parameters(), lr=self.model_lr)
+        self.optimizer_head = torch.optim.Adam(self.classifier.parameters(), lr=self.classifier_lr)
+        self.losses = []
+        self.evaluation_reports = []
+        if type(self.text_field) is str:
+            self.text_field = [self.text_field]
+
+    def save(self):
+        with open(f'models/{self.identifier_str}-losses.dat', 'wb') as f:
+            pickle.dump(self.losses, f)
+        with open(f'models/{self.identifier_str}-evaluations.dat', 'wb') as f:
+            pickle.dump(self.evaluation_reports, f)
+        #self.model.save_pretrained(f'models/{self.identifier_str}-model.dat')
+    
+    def forward(self, tokenized_texts):
+        model_outputs = [self.model.forward(**text).pooler_output for text in tokenized_texts]
+        logits = self.classifier(torch.cat(model_outputs, dim=1))
+        return logits
+    
+    def tokenize(self, batch):
+        batch_text = [list(batch[field]) for field in self.text_field]
+        return [self.tokenizer(text, truncation=True, return_tensors='pt', padding=True).to(device) for text in batch_text]
+
+    
+    def evaluate(self, split):
+        self.model.eval()
+        ds_split = self.ds[split]
+        predictions = []
+        for batch in ds_split.batch(batch_size=16):
+            tokenized_texts = self.tokenize(batch)
+            logits = self.forward(tokenized_texts)
+            prediction = torch.argmax(logits, dim=1)
+            predictions.append(prediction)
+        all_predictions = torch.cat(predictions).cpu().numpy()
+
+        print(classification_report(y_true=ds_split[self.label_field], y_pred=all_predictions))
+        report_dict = classification_report(y_true=ds_split[self.label_field], y_pred=all_predictions, output_dict=True)
+        self.evaluation_reports.append(report_dict)
+        self.model.train()
+
+
+    def train(self, epochs, batch_size=16):
+        self.evaluate(self.test_split)
+        for epoch in range(epochs):
+            print(f'--------------------- Epoch {epoch} ---------------------')
+            episode_ds = self.ds[self.train_split].shuffle()
+            for step, batch in enumerate(episode_ds.batch(batch_size=batch_size)):
+                tokenized_texts = self.tokenize(batch)
+                labels = torch.tensor(batch[self.label_field]).to(device)
+
+                self.optimizer_network.zero_grad()
+                self.optimizer_head.zero_grad()
+
+                logits = self.forward(tokenized_texts)
+                loss = self.loss_func(logits, labels)
 
                 loss.backward()
                 self.optimizer_network.step()
@@ -48,4 +168,6 @@ class ClassificationTrainer:
 
                 print(step, loss.item(), end='\r')
                 self.losses.append(loss.item())
+            
+            self.evaluate(self.test_split)
             self.save()
--- Classification_training/training_script.py
+++ Classification_training/training_script.py
@@ -1,52 +1,100 @@
 import torch
+import os
 from datasets import load_from_disk
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification
 
 from Trainer import ClassificationTrainer
-from Evaluation import Classification_Evaluation
 
-# -----------------------------------------------
 
-IDENTIFIER_STR = 'yelp_4eps_1-losses' #0-2_CoT_4eps_1'
+print('======================================================')
 
-DATASET = "Scientific-text-classification-preprocessed" #"yelp_ds_preprocessed"
-LABEL_FIELD = 'label_idx'
+IDENTIFIER_STR = os.environ["C_IDENTIFIER_STR"]
+print('IDENTIFIER_STR:', IDENTIFIER_STR)
 
-MODEL_OUT = 10
+DATASET = os.environ.get('C_DATASET')
+print('DATASET:', DATASET)
 
-WORK_DIR = '/mnt/lustre/work/eickhoff/esx833/NLP_project/'
-MODEL_DIR = 'CoT_training/models/0-2-model.dat'
-MODEL_PATH = "bert-base-uncased" # WORK_DIR + MODEL_DIR
+EVALUATION_DATASETS = [model.split() for model in os.environ.get('C_EVALUATION_DATASETS').split('\n') if len(model)>0]
+print('EVALUATION_DATASETS:', EVALUATION_DATASETS)
 
-# -----------------------------------------------
+TEXT_FIELD = os.environ.get('C_TEXT_FIELD')
+print('TEXT_FIELD:', TEXT_FIELD)
+
+LABEL_FIELD = os.environ.get('C_LABEL_FIELD')
+print('LABEL_FIELD:', LABEL_FIELD)
+
+EPOCHS = int(os.environ.get('C_EPOCHS'))
+print('EPOCHS:', EPOCHS)
+
+LR_MODEL = int(os.environ.get('C_LR_MODEL'))
+print('LR_MODEL:', LR_MODEL)
+
+LR_CLASSIFIER = int(os.environ.get('C_LR_CLASSIFIER'))
+print('LR_CLASSIFIER:', LR_CLASSIFIER)
+
+NUM_HIDDEN_LAYERS = int(os.environ.get('C_NUM_HIDDEN_LAYERS'))
+OUTPUT_FEATURES = int(os.environ.get('C_OUTPUT_FEATURES'))
+HIDDEN_NODES = 768*len(TEXT_FIELD)
+CLASSIFIER = torch.nn.Sequential(
+    *(torch.nn.Linear(in_features=HIDDEN_NODES, out_features=HIDDEN_NODES, bias=True) for _ in range(NUM_HIDDEN_LAYERS)),
+    torch.nn.Linear(in_features=HIDDEN_NODES, out_features=OUTPUT_FEATURES, bias=True)
+)
+print('CLASSIFIER:', CLASSIFIER)
+
+MODELS = [model.split() for model in os.environ.get('C_MODELS').split('\n') if len(model)>0]
+print('MODELS:', MODELS)
+
+print('======================================================')
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_default_device(device)
 print('Device:', device)
 
-dataset = load_from_disk("datasets/" + DATASET)
+dataset = load_from_disk(DATASET)
 print(dataset, dataset['train'][0])
 
+evaluation_datasets = [(name, load_from_disk(path)[split]) for name, split, path in EVALUATION_DATASETS]
+evaluation_datasets.append(('test_split', dataset['test']))
+print(evaluation_datasets)
+for name, d in evaluation_datasets:
+    print(name, set(d['label']))
+print('train_split', set(dataset['train']['label']))
+
 tok: BertTokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-classification_model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
-classification_model.classifier = torch.nn.Linear(in_features=768, out_features=MODEL_OUT, bias=True)
 
-classification_trainer = ClassificationTrainer(
-    model=classification_model,
-    ds=dataset['train'],
-    tokenizer=tok,
-    identifier_str=IDENTIFIER_STR,
-    label_field=LABEL_FIELD
-)
+for model, path in MODELS:
+    print('======================================================')
+    print(model, path)
+    id_str = IDENTIFIER_STR.replace('MODEL', model)
+    print(id_str)
+    classification_model = BertForSequenceClassification.from_pretrained(path)
+    print('======================================================')
 
-classification_trainer.train(4, batch_size=32)
+    classification_trainer = ClassificationTrainer(
+        model=classification_model,
+        ds=dataset,
+        tokenizer=tok,
+        identifier_str=id_str,
+        text_field=TEXT_FIELD,
+        label_field=LABEL_FIELD,
+        evaluation_datasets=evaluation_datasets,
+        model_lr=10**(-LR_MODEL) if LR_MODEL != 0 else 0,
+        classifier_lr=10**(-LR_CLASSIFIER)
+    )
 
-classification_evaluation = Classification_Evaluation(
-    model=classification_model,
-    ds=dataset,
-    tokenizer=tok,
-)
-print('Train evaluation')
-classification_evaluation.evaluate('train')
-print('Test evaluation')
-classification_evaluation.evaluate('test')
+    classification_trainer.train(EPOCHS, batch_size=32)
+
+    print('======================================================')
+
+# classification_evaluation = Classification_Evaluation(
+#     model=classification_model,
+#     ds=dataset,
+#     tokenizer=tok,
+#     identifier_str=IDENTIFIER_STR,
+#     label_field=LABEL_FIELD
+# )
+# print('Train evaluation')
+# classification_evaluation.evaluate('train')
+# print('Test evaluation')
+# classification_evaluation.evaluate('test')
